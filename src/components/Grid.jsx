fdc649f0b89220099d89422691eb382edcb75a45
Michał Żaloudik <ponury.kostek@gmail.com>
2018-01-09 10:48:12 +0100

    Draft

---
+++ src/components/Grid.jsx
@@ -0,0 +1,41 @@
+/**
+ * @author Michał Żaloudik <ponury.kostek@gmail.com>
+ */
+"use strict";
+import React, {Component} from "react";
+
+class Grid extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	componentDidMount() {
+	}
+
+	componentWillUnmount() {
+	}
+
+	componentWillReceiveProps(nextProps) {
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+		return true;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+	}
+
+	componentDidCatch(error, info) {
+	}
+
+	render() {
+		return null;
+	}
+}
+
+Grid.defaultProps = {};
+Grid.displayName = "";
+export default Grid;
---
+++ src/plugins/Theme.js
@@ -0,0 +1,11 @@
+/**
+ * @author Michał Żaloudik <ponury.kostek@gmail.com>
+ */
+"use strict";
+class Theme {
+	constructor(props) {}
+	load() {}
+	get(key) {}
+}
+const theme = new Theme();
+export default
